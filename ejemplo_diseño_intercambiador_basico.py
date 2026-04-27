4cf9a63fba6be65a8c4ffda6ae38edb58c5a8d22
Whady Felipe Florez <whady.florez@upb.edu.co>
2026-04-27 08:48:29 -0500

    update

--- ejemplo_diseño_intercambiador_basico.py
+++ ejemplo_diseño_intercambiador_basico.py
@@ -10,26 +10,33 @@ mh=2
 cph=3500
 Thi=80
 Tho=50
-Ch=mh*cph
-
-q=Ch*(Thi-Tho)
 Tci=15
 mc=2.5
+U=2000
 cpc=4180
-Cc=mc*cpc
-Tco=q/Cc+Tci
+D=2e-2 
 
+Ch=mh*cph
+Cc=mc*cpc
+
+q=Ch*(Thi-Tho)
+
+
+#flujo pararlelo
+Tco=q/Cc+Tci
 dT1=Thi-Tci
 dT2=Tho-Tco
 DTln=(dT1-dT2)/np.log(dT1/dT2)
 
-U=2000
+
 A=q/(DTln*U)
 
-D=2e-2 
+
 L=A/(np.pi*D)
 pasos=L/7
 
+
+#contraflujo
 dT1cf=Thi-Tco
 dT2cf=Tho-Tci
 DTlncf=(dT1cf-dT2cf)/np.log(dT1cf/dT2cf)
@@ -38,6 +45,8 @@ Acf=q/(DTlncf*U)
 Lcf=Acf/(np.pi*D)
 pasoscf=Lcf/7
 
+
+#NTU contraflujo
 Cmin=Ch
 eff=q/(Cmin*(Thi-Tci))
 Cr=Cmin/Cc
